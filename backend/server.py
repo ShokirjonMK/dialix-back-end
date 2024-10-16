3ba476ad968c47d500f61592afd37290e6423dce
Abduaziz Ziyodov <abduaziz.ziyodov@mail.ru>
2024-10-16 20:36:01 +0500

    Made healthcheck endpoint somewhat useful

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,16 @@
 import os
 import sys
+import logging
 import datetime
 from platform import node as get_hostname
 
-from fastapi import FastAPI, status
+from tortoise import connections
 
+from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-from backend.api import api_router
+# from backend.api import api_router
 from backend.sockets import sio_app
 from backend.core.lifespan import lifespan_handler
 from backend.core.logging import configure_logging
@@ -26,7 +28,7 @@ register_exception_handlers(application)
 
 application.mount("/ws/", app=sio_app)
 
-application.include_router(api_router)
+# application.include_router(api_router)
 application.include_router(user_router)
 application.include_router(checklist_router)
 
@@ -52,12 +54,22 @@ application.add_middleware(
 
 
 @application.get("/health")
-def health():
+async def health():
+    connection = connections.get("default")
+    db_version_record = await connection.execute_query("select version();")
+
+    try:
+        db_version = db_version_record[1][0]["version"]
+    except Exception as exc:
+        logging.error(f"{db_version_record=} {exc=}")
+        db_version = None
+
     return JSONResponse(
         status_code=status.HTTP_200_OK,
         content={
-            "status": "ok",
+            "database": db_version,
             "hostname": get_hostname(),
             "time": datetime.datetime.now().isoformat(),
+            "status": "!ok" if db_version is None else "ok",
         },
     )
