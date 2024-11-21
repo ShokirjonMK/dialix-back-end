ceb575bdd1c85df14444dca1395f8635c5106a31
Abduaziz Ziyodov <abduaziz.ziyodov@mail.ru>
2024-11-21 13:26:34 +0500

    File cleanup solution after uploading on analyze endpoint

--- backend/tasks/pbx.py
+++ backend/tasks/pbx.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import typing as t  # noqa: F401
 
@@ -28,50 +29,59 @@ def process_pbx_call_task(*args, **kwargs):
 
     url = f"{settings.PBX_API_URL.format(domain=domain)}/mongo_history/search.json"
 
-    call_info_response = sync_get_call_info_by(uuid, url, key_id, key)
+    try:
+        call_info_response = sync_get_call_info_by(uuid, url, key_id, key)
+        if int(call_info_response["status"]) == 0:
+            logging.error("Invalid credentials.")
+            return {"error": call_info_response["comment"]}
 
-    if int(call_info_response["status"]) == 0:
-        logging.error("Invalid credentials.")
-        return {"error": call_info_response["comment"]}
+        call_info = call_info_response["data"][0]
 
-    call_info = call_info_response["data"][0]
+        if call_info["user_talk_time"] == 0:
+            logging.error("Call has no user talk time.")
+            return {"error": "User did not say any word, and we need to analyze that?"}
 
-    if call_info["user_talk_time"] == 0:
-        logging.error("Call has no user talk time.")
-        return {"error": "User did not say any word, and we need to analyze that?"}
+        download_url_response = sync_get_call_download_url(
+            {"uuid": uuid, "download": "1"},
+            url,
+            key_id,
+            key,
+        )
+        download_url = download_url_response.get("data")
+        filename = f"call-{uuid}.mp3"
+        download_file_path = f"./audios/{filename}"
 
-    download_url_response = sync_get_call_download_url(
-        {"uuid": uuid, "download": "1"},
-        url,
-        key_id,
-        key,
-    )
-    download_url = download_url_response.get("data")
-    filename = f"call-{uuid}.mp3"
-    download_file_path = f"./audios/{filename}"
+        downloaded_file = sync_download_from(download_url, download_file_path)
+        logging.info(f"Downloaded file path: {downloaded_file}")
 
-    downloaded_file = sync_download_from(download_url, download_file_path)
-    logging.info(f"Downloaded file path: {downloaded_file}")
+        with open(downloaded_file, "rb") as audio_file:
+            upload_file = UploadFile(audio_file, filename=filename)
 
-    with open(downloaded_file, "rb") as audio_file:
-        upload_file = UploadFile(audio_file, filename=filename)
+            processed_data = (
+                [upload_file],
+                [True],
+                [checklist_id],
+                [{"file_path": download_file_path, "duration": call_info["duration"]}],
+            )
 
-    processed_data = (
-        [upload_file],
-        [True],
-        [checklist_id],
-        [{"file_path": download_file_path, "duration": call_info["duration"]}],
-    )
+            current_user = get_user_by_id(next(get_db_session()), user_id)
+            response = analyze_data(
+                processed_data=processed_data,
+                current_user=current_user,
+                _operator_code=call_info["caller_id_number"],
+                _call_type=call_info["accountcode"],
+                _destination_number=call_info["destination_number"],
+            )
 
-    current_user = get_user_by_id(next(get_db_session()), user_id)
-    response = analyze_data(
-        processed_data=processed_data,
-        current_user=current_user,
-        _operator_code=call_info["caller_id_number"],
-        _call_type=call_info["accountcode"],
-        _destination_number=call_info["destination_number"],
-    )
+            logging.info(f"Analysis endpoint's response: {response}")
 
-    logging.info(f"Analysis endpoint's response: {response}")
-
-    return {"success": True, "response": response.status_code}
+        return {"success": True, "response": response.status_code}
+    finally:
+        if os.path.exists(download_file_path):
+            try:
+                os.remove(download_file_path)
+                logging.info(f"Deleted file: {download_file_path}")
+            except Exception as exc:
+                logging.error(
+                    f"Failed to delete file: {download_file_path}. Error: {exc}"
+                )
