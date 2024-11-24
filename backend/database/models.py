d367598e333200c90b5064ac68f900dfa5509848
Abduaziz Ziyodov <abduaziz.ziyodov@mail.ru>
2024-11-24 23:58:51 +0500

    Feature: automatically insert bitrix result at the end of audio processing

--- backend/database/models.py
+++ backend/database/models.py
@@ -76,7 +76,7 @@ class Checklist(Base):
     owner_id = Column(
         PostgresUUID(as_uuid=True), ForeignKey("account.id"), nullable=False
     )
-    title = Column(String, nullable=False, unique=True)
+    title = Column(String, nullable=False)
     payload = Column(JSON)
     active = Column(Boolean, server_default="false", nullable=False)
     created_at = Column(TIMESTAMP, server_default=text("now()"), nullable=False)
--- backend/utils/bitrix.py
+++ backend/utils/bitrix.py
@@ -1,7 +1,7 @@
 import httpx
 import logging
 import typing as t
-from datetime import datetime
+# from datetime import datetime
 
 STAGE_LIST_URL: str = "{}/crm.status.list"
 DEAL_LIST_URL: str = "{}/crm.deal.list"
@@ -11,11 +11,9 @@ CONTACT_LIST_URL: str = "{}/crm.contact.list"
 BITRIX_DEAL_CREATED_TIME_FORMAT: str = "%Y-%m-%dT%H:%M:%S%z"
 
 
-async def get_deals_by_phone(webhook_url: str, phone_number: str) -> list[dict]:
-    async with httpx.AsyncClient(timeout=30) as client:
-        contact_info = await get_contact_info_by_phone(
-            webhook_url, client, phone_number
-        )
+def get_deals_by_phone(webhook_url: str, phone_number: str) -> list[dict]:
+    with httpx.Client(timeout=30) as client:
+        contact_info = get_contact_info_by_phone(webhook_url, client, phone_number)
 
         if not contact_info:
             logging.info(f"No contact found for phone number: {phone_number}")
@@ -25,26 +23,19 @@ async def get_deals_by_phone(webhook_url: str, phone_number: str) -> list[dict]:
         contact_name = contact_info["NAME"]
 
         logging.info(
-            f"Retrieved contact: {contact_name} ({contact_id}) for {phone_number=}"
+            f"Retrieved contact: {contact_name=} ({contact_id=}) for {phone_number=}"
         )
 
-        deals = await get_deals_by_contact_id(webhook_url, client, contact_id)
+        deals = get_deals_by_contact_id(webhook_url, client, contact_id)
 
         if not deals:
-            logging.info(f"No deals found for contact ID: {contact_id}")
+            logging.warning(f"No deals found for contact ID: {contact_id}")
             return contact_name, []
 
         result: list[dict] = []
 
         for deal in deals:
-            logging.info(f"{deal=}")
             deal_stage: str = deal.get("STAGE_ID", "Unknown")
-
-            logging.info(
-                f"Deal ID: {deal['ID']}, Title: {deal['TITLE']}, "
-                f"Status: {deal_stage}, Created: {deal['DATE_CREATE']}, "
-                f"Closed: {deal.get('CLOSEDATE', 'N/A')}"
-            )
 
             date_created = deal["DATE_CREATE"]
             date_closed = deal.get("CLOSEDATE")
@@ -54,14 +45,8 @@ async def get_deals_by_phone(webhook_url: str, phone_number: str) -> list[dict]:
                     "deal_id": deal["ID"],
                     "title": deal["TITLE"],
                     "status": deal_stage.lower(),
-                    "date_created": datetime.strptime(
-                        date_created, BITRIX_DEAL_CREATED_TIME_FORMAT
-                    ),
-                    "date_closed": datetime.strptime(
-                        date_closed, BITRIX_DEAL_CREATED_TIME_FORMAT
-                    )
-                    if date_closed
-                    else None,
+                    "date_created": date_created,
+                    "date_closed": date_closed if date_closed else None,
                 }
             )
 
@@ -70,12 +55,12 @@ async def get_deals_by_phone(webhook_url: str, phone_number: str) -> list[dict]:
         return contact_name, result
 
 
-async def get_deals_by_contact_id(
-    webhook_url: str, client: httpx.AsyncClient, contact_id: str
+def get_deals_by_contact_id(
+    webhook_url: str, client: httpx.Client, contact_id: str
 ) -> list[dict]:
-    stage_mapping = await get_stage_mapping(webhook_url, client)
+    stage_mapping = get_stage_mapping(webhook_url, client)
 
-    response = await client.get(
+    response = client.get(
         DEAL_LIST_URL.format(webhook_url),
         params={
             "filter[CONTACT_ID]": contact_id,
@@ -95,11 +80,11 @@ async def get_deals_by_contact_id(
     return deals
 
 
-async def get_contact_info_by_phone(
-    webhook_url: str, client: httpx.AsyncClient, phone_number: str
+def get_contact_info_by_phone(
+    webhook_url: str, client: httpx.Client, phone_number: str
 ) -> t.Optional[dict]:
-    async def make_contact_list_request(client_phone_number: str):
-        response = await client.get(
+    def make_contact_list_request(client_phone_number: str):
+        response = client.get(
             CONTACT_LIST_URL.format(webhook_url),
             params={
                 "filter[PHONE]": client_phone_number,
@@ -115,7 +100,7 @@ async def get_contact_info_by_phone(
 
         return contacts[0]
 
-    first_try = await make_contact_list_request(phone_number)
+    first_try = make_contact_list_request(phone_number)
 
     if first_try:
         return first_try
@@ -144,11 +129,11 @@ async def get_contact_info_by_phone(
         f"Made request with {phone_number=}, it contains {phone_number_digits} digits. "
         f"Making SECOND request with {other_format_phone_number}"
     )
-    second_try = await make_contact_list_request(other_format_phone_number)
+    second_try = make_contact_list_request(other_format_phone_number)
     return second_try
 
 
-async def get_stage_mapping(webhook_url: str, client: httpx.AsyncClient) -> dict:
-    response = await client.get(STAGE_LIST_URL.format(webhook_url))
+def get_stage_mapping(webhook_url: str, client: httpx.Client) -> dict:
+    response = client.get(STAGE_LIST_URL.format(webhook_url))
     statuses = response.json().get("result", [])
     return {status["STATUS_ID"]: status["NAME"] for status in statuses}
